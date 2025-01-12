958b6c581585718e084f535941a6f9691d279461
emteg@mailbox.org <emteg@mailbox.org>
2025-01-12 15:27:56 +0100

    Added Cell2D class and some tests; updated .gitignore

---
+++ csharp/Common.Tests/Cell2DShould.cs
@@ -0,0 +1,63 @@
+﻿namespace Common.Tests;
+
+public class Cell2DShould
+{
+    [Fact]
+    public void HaveAllItsFourNeighborsSetToNull_WhenCreating()
+    {
+        Cell2D<byte> cell = new();
+        
+        Assert.Null(cell.Up);
+        Assert.Null(cell.Right);
+        Assert.Null(cell.Down);
+        Assert.Null(cell.Left);
+    }
+
+    [Fact]
+    public void HaveTheCorrectNeighbor_AndConnectBothWays_WhenConnectingToAnotherCell()
+    {
+        Cell2D<byte> cell = new();
+        Cell2D<byte> upNeighbor = new();
+        Cell2D<byte> rightNeighbor = new();
+        Cell2D<byte> downNeighbor = new();
+        Cell2D<byte> leftNeighbor = new();
+
+        cell.ConnectUp(upNeighbor);
+        cell.ConnectRight(rightNeighbor);
+        cell.ConnectDown(downNeighbor);
+        cell.ConnectLeft(leftNeighbor);
+
+        Assert.Equal(upNeighbor, cell.Up);
+        Assert.Equal(cell, upNeighbor.Down);
+        Assert.Equal(rightNeighbor, cell.Right);
+        Assert.Equal(cell, rightNeighbor.Left);
+        Assert.Equal(downNeighbor, cell.Down);
+        Assert.Equal(cell, downNeighbor.Up);
+        Assert.Equal(leftNeighbor, cell.Left);
+        Assert.Equal(cell, leftNeighbor.Right);
+    }
+    
+    [Fact]
+    public void NotConnectBack_WhenConnectingToAnotherCell_GivenNotBothWays()
+    {
+        Cell2D<byte> cell = new();
+        Cell2D<byte> upNeighbor = new();
+        Cell2D<byte> rightNeighbor = new();
+        Cell2D<byte> downNeighbor = new();
+        Cell2D<byte> leftNeighbor = new();
+
+        cell.ConnectUp(upNeighbor, false);
+        cell.ConnectRight(rightNeighbor, false);
+        cell.ConnectDown(downNeighbor, false);
+        cell.ConnectLeft(leftNeighbor, false);
+
+        Assert.Equal(upNeighbor, cell.Up);
+        Assert.Equal(rightNeighbor, cell.Right);
+        Assert.Equal(downNeighbor, cell.Down);
+        Assert.Equal(leftNeighbor, cell.Left);
+        Assert.Null(upNeighbor.Down);
+        Assert.Null(rightNeighbor.Left);
+        Assert.Null(downNeighbor.Up);
+        Assert.Null(leftNeighbor.Right);
+    }
+}
---
+++ csharp/Common/Cell2D.cs
@@ -0,0 +1,38 @@
+﻿namespace Common;
+
+public class Cell2D<T>
+{
+    public T? Payload { get; set; } = default;
+    public Cell2D<T>? Up { get; private set; }
+    public Cell2D<T>? Right { get; private set; }
+    public Cell2D<T>? Down { get; private set; }
+    public Cell2D<T>? Left { get; private set; }
+
+    public void ConnectUp(Cell2D<T> up, bool bothWays = true)
+    {
+        Up = up;
+        if (bothWays)
+            up.Down = this;
+    }
+    
+    public void ConnectRight(Cell2D<T> right, bool bothWays = true)
+    {
+        Right = right;
+        if (bothWays)
+            right.Left = this;
+    }
+    
+    public void ConnectDown(Cell2D<T> down, bool bothWays = true)
+    {
+        Down = down;
+        if (bothWays)
+            down.Up = this;
+    }
+    
+    public void ConnectLeft(Cell2D<T> left, bool bothWays = true)
+    {
+        Left = left;
+        if (bothWays)
+            left.Right = this;
+    }
+}
