17a77e10e32f39746ea617865cadcd813c6ba646
Matthias Grabowski <matthias.grabowksi@theobald-software.com>
2025-06-12 11:42:36 +0200

    Finished 2015/Day05/Part2

--- csharp/2015/Day05/Program.cs
+++ csharp/2015/Day05/Program.cs
@@ -6,56 +6,80 @@ public static class Program
 {
     public static void Main()
     {
-        Console.WriteLine(IsNice("ugknbfddgicrmopn"));
-        Console.WriteLine(IsNice("aaa"));
-        Console.WriteLine(IsNice("jchzalrnumimnmhp"));
-        Console.WriteLine(IsNice("haegwjzuvuyypxyu"));
-        Console.WriteLine(IsNice("dvszwmarrgswjxmb"));
-
-        Console.WriteLine(FileReader.ReadLines("input.txt").Count(IsNice));
+        Console.WriteLine(ExpectIsNice(true, "ugknbfddgicrmopn", IsNicePart1));
+        Console.WriteLine(ExpectIsNice(true, "aaa", IsNicePart1));
+        Console.WriteLine(ExpectIsNice(false, "jchzalrnumimnmhp", IsNicePart1));
+        Console.WriteLine(ExpectIsNice(false, "haegwjzuvuyypxyu", IsNicePart1));
+        Console.WriteLine(ExpectIsNice(false, "dvszwmarrgswjxmb", IsNicePart1));
+        Console.WriteLine();
+        Console.WriteLine($"Part 1: {FileReader.ReadLines("input.txt").Count(IsNicePart1)}");
+        Console.WriteLine();
+        Console.WriteLine(ExpectIsNice(true, "qjhvhtzxzqqjkmpb", IsNicePart2));
+        Console.WriteLine(ExpectIsNice(true, "xxyxx", IsNicePart2));
+        Console.WriteLine(ExpectIsNice(false, "uurcxstgmygtbstg", IsNicePart2));
+        Console.WriteLine(ExpectIsNice(false, "ieodomkazucvgmuy", IsNicePart2));
+        Console.WriteLine();
+        Console.WriteLine($"Part 2: {FileReader.ReadLines("input.txt").Count(IsNicePart2)}");
     }
 
-    private static bool IsNice(string s)
+    private static string ExpectIsNice(bool expectation, string s, Func<string, bool> action)
     {
-        uint vowels = 0;
-        bool has3Vowels = false;
-        bool hasDoubleLetter = false;
-        char lastChar = '\0';
-        bool firstChar = true;
-        foreach (char c in s)
+        return $"Expecting string '{s}' to be {(expectation ? "nice" : "naughty")} and it is {(action(s) ? "NICE" : "NAUGHTY")}";
+    }
+
+    private static bool IsNicePart2(string s)
+    {
+        return ContainsPairsOfAnyTwoLettersAtLeastTwice(s) && ContainsAtLeastOneSymmetricTriple(s);
+    }
+
+    private static bool ContainsPairsOfAnyTwoLettersAtLeastTwice(string s)
+    {
+        for (int i = 0; i < s.Length - 3; i++)
         {
-            if (!has3Vowels && IsVowel(c))
+            string pair = s.Substring(i, 2);
+            for (int j = i + 2; j < s.Length - 1; j++)
             {
-                vowels++;
-                if (vowels >= 3)
-                    has3Vowels = true;
+                string other = s.Substring(j, 2);
+                if (pair == other) 
+                    return true;
             }
-
-            if (firstChar)
-            {
-                firstChar = false;
-                lastChar = c;
-                continue;
-            }
-
-            if (!hasDoubleLetter && lastChar == c)
-            {
-                hasDoubleLetter = true;
-            }
-
-            if ((lastChar == 'a' && c == 'b') ||
-                (lastChar == 'c' && c == 'd') ||
-                (lastChar == 'p' && c == 'q') || 
-                (lastChar == 'x' && c == 'y'))
-            {
-                return false;
-            }
-
-            firstChar = false;
-            lastChar = c;
         }
 
-        return has3Vowels && hasDoubleLetter;
+        return false;
+    }
+
+    private static bool ContainsAtLeastOneSymmetricTriple(string s)
+    {
+        for (int i = 0; i < s.Length - 2; i++)
+        {
+            string triple = s.Substring(i, 3);
+            if (triple[0] == triple[2])
+                return true;
+        }
+        return false;
+    }
+
+    private static bool IsNicePart1(string s)
+    {
+        return DoesNotContainAnyBadPairs(s) && ContainsAtLeastThreeVowels(s) && ContainsAtLeastOnePair(s);
+    }
+
+    private static bool ContainsAtLeastThreeVowels(string s) => s.Count(IsVowel) >= 3;
+
+    private static bool ContainsAtLeastOnePair(string s)
+    {
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            if (s[i] == s[i + 1])
+                return true;
+        }
+
+        return false;
+    }
+
+    private static bool DoesNotContainAnyBadPairs(string s)
+    {
+        return !s.Contains("ab") && !s.Contains("cd") && !s.Contains("pq") && !s.Contains("xy");
     }
 
     private static bool IsVowel(char c) => c is 'a' or 'e' or 'i' or 'o' or 'u';
